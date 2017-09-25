24a39d36480f7e00d4b4e0d53f6a619201dc789b
Özgür Kaya <okaya.gm@gmail.com>
2017-09-25 13:45:48 +0300

    2.commit

--- formul.js
+++ formul.js
@@ -1,27 +1,58 @@
-window.onload = function(){
+$(document).ready(function(){
 
-
-
-var alfabe = ["A","B","C","Ç","D","E","F","G"]
-
-for(var i = 0; i< alfabe.length;i++){
-
-    var buton = document.createElement("button");
-    buton.id = "buton"+i;
-    buton.innerHTML = alfabe[i];
-    buton.style.margin = "10px" ;
-    buton.style.fontSize = "25px";
-    buton.onclick = islem();
+    var abc = "A B C Ç D E F G Ğ H İ I J K L M N O Ö P R S Ş T U Ü V Y Z";
+    var klm = prompt("kelimeyi giriniz..")
     
-    document.body.appendChild(buton);
+    var kelime = klm.split("");
+
+        var alfabe = abc.split(" ");
+    
+        for (var i = 0; i < alfabe.length; i++) {
+    
+            var buton = document.createElement("button");
+    
+            buton.innerHTML = alfabe[i];
+                       
+            document.getElementById('ust').appendChild(buton);
+            buton.onclick = function () {
+
+              
+                    $(this).css("background", "red");
+                
+                
+            };
+    
+    
+        };
+
+        for (var i = 0; i < kelime.length; i++) {
+           
+                 var buton = document.createElement("button");
+            
+                    buton.innerHTML = kelime[i];
+                                        
+                    document.getElementById('ortasol').appendChild(buton);
+                    buton.onclick = function () {
+        
+                      
+                            $(this).css("background", "red");
+                        
+                        
+                    };
+            
+            
+        }
     
 
-}
+});
 
-function islem() {
- buton.style.backgroundColor = "yellow";
-
-}
+   
 
 
-};
+
+
+
+
+
+
+

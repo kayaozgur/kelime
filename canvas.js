c03604cd968f0ae10bda1b9674c0274dc2e79953
Özgür Kaya <okaya.gm@gmail.com>
2017-09-26 16:39:54 +0300

    111

--- canvas.js
+++ canvas.js
@@ -7,10 +7,21 @@ $(document).ready(function(){
     function kafa() {
 
         ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.arc(50, 50, 30, 0 * Math.PI, 2 * Math.PI);
-        ctx.fill();
+        ctx.strokeStyle = "red";
+        ctx.arc(100, 100, 30, 0 * Math.PI, 2 * Math.PI);
+        ctx.moveTo(100,150);
+        ctx.lineTo(100,250);
+        ctx.stroke();
     }
+
+    function govde() {
+            
+                    ctx.beginPath();
+                    ctx.strokeStyle = "red";
+                    ctx.moveTo(100,130);
+                    ctx.lineTo(100,250);
+                    ctx.stroke();
+                }
 
 
 

aa8aac5b13a58846dc220d37473dd8122f74b7b3
Özgür Kaya <okaya.gm@gmail.com>
2017-09-26 17:57:05 +0300

    26/09/2017

--- butonlar.js
+++ butonlar.js
@@ -1,56 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var abc = "A B C Ç D E F G Ğ H İ I J K L M N O Ö P R S Ş T U Ü V Y Z";
-    var klm = prompt("kelimeyi giriniz..")
-    
-    var kelime = klm.split("");
+        var abc = "A B C Ç D E F G Ğ H İ I J K L M N O Ö P R S Ş T U Ü V Y Z";
+        var klm = prompt("kelimeyi giriniz..")
+
+        var kelime = klm.split("");
 
         var alfabe = abc.split(" ");
-    
-        for (var i = 0; i < alfabe.length; i++) {
-    
-            var buton = document.createElement("button");
-    
-            buton.innerHTML = alfabe[i];
-                       
-            document.getElementById('ust').appendChild(buton);
-            buton.onclick = function () {
 
-              
-                    $(this).css("background", "red");
-                  
-                
-                
-            };
-    
-    
+        for (var i = 0; i < alfabe.length; i++) {
+
+                var butonAbc = document.createElement("button");
+
+                butonAbc.innerHTML = alfabe[i];
+
+                document.getElementById('ust').appendChild(butonAbc);
+                butonAbc.onclick = function () {
+
+                        $(this).css("background", "red");
+                        var harf, index;
+                        harf = $(this).text();
+                        index = klm.indexOf(harf);
+                        
+                        if(index != -1) {
+                                $('#'+index).text(harf);
+                        }
+                        
+
+
+                };
+
+
         };
 
         for (var i = 0; i < kelime.length; i++) {
-           
-                 var buton = document.createElement("button");
-             
-                    buton.innerHTML = kelime[i];
-                    if (buton.getAttribute == "") { buton.style.background = "red";}                   
-                    document.getElementById('ortasol').appendChild(buton);
-                    buton.onclick = function () {
-        
-                      
-                            $(this).css("background", "red");
-                            
-                        
-                        
-                    };
-            
-            
+
+                var butonKelime = document.createElement("button");
+                butonKelime.id = i;
+                if (kelime[i] == " ") {
+                        butonKelime.style.background = "black";
+
+                }
+
+                document.getElementById('ortasol').appendChild(butonKelime);
+
+
         }
-      
-        kafa();
-        govde();
+
+        
 
 });
 
-   
+
 
 
 
